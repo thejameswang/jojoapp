4e4fb3a835eceeeb4925db028c013d2866557fec
James Wang <j.wang5441@gmail.com>
2018-02-26 17:24:12 -0800

    creates foundation for code

---
+++ App.js
@@ -0,0 +1,121 @@
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+  TextInput,
+  ListView,
+  Alert,
+  Button
+} from 'react-native';
+import { StackNavigator } from 'react-navigation';
+
+
+//Screens
+class LoginScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Login'
+  };
+
+  press() {
+
+  }
+  register() {
+    this.props.navigation.navigate('Register');
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.textBig}>Login to HoHoHo!</Text>
+        <TouchableOpacity onPress={ () => {this.press()} } style={[styles.button, styles.buttonGreen]}>
+          <Text style={styles.buttonLabel}>Tap to Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={[styles.button, styles.buttonBlue]} onPress={ () => {this.register()} }>
+          <Text style={styles.buttonLabel}>Tap to Register</Text>
+        </TouchableOpacity>
+      </View>
+    )
+  }
+}
+
+class RegisterScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Register'
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.textBig}>Register</Text>
+      </View>
+    )
+  }
+}
+
+
+//Navigator
+export default StackNavigator({
+  Login: {
+    screen: LoginScreen,
+  },
+  Register: {
+    screen: RegisterScreen,
+  },
+}, {initialRouteName: 'Login'});
+
+
+//Styles
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  containerFull: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'stretch',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  textBig: {
+    fontSize: 36,
+    textAlign: 'center',
+    margin: 10,
+  },
+  button: {
+    alignSelf: 'stretch',
+    paddingTop: 10,
+    paddingBottom: 10,
+    marginTop: 10,
+    marginLeft: 5,
+    marginRight: 5,
+    borderRadius: 5
+  },
+  buttonRed: {
+    backgroundColor: '#FF585B',
+  },
+  buttonBlue: {
+    backgroundColor: '#0074D9',
+  },
+  buttonGreen: {
+    backgroundColor: '#2ECC40'
+  },
+  buttonLabel: {
+    textAlign: 'center',
+    fontSize: 16,
+    color: 'white'
+  }
+});
